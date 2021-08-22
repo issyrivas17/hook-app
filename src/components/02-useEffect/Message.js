5d224f0f73be6d190165232957deee82c8957338
issyrivas17 <ismeniarivas17@gmail.com>
2021-08-22 17:55:52 -0400

    UseEffect y sus precacuciones para no consumir memoria

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,11 +1,19 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react'
 
-export const Message = () => {
+export const Message = () => { 
 
+     const [coords, setCoords] = useState({x:0 , y:0})
+    const {x,y} = coords;   
     useEffect(() => {
-        console.log('Componente montado')
+
+        const mouseMove = (e) => {
+               const coords = {x:e.x, y:e.y}; 
+          setCoords(coords)  
+
+        }
+       window.addEventListener('mousemove',mouseMove) 
         return () => {
-            console.log('Componente desmontado')
+            window.removeEventListener('mousemove',mouseMove); 
         };
     }, []);
 
@@ -13,6 +21,7 @@ export const Message = () => {
     return (
         <div>
            <h3>Eres genial!! </h3> 
+           <p>x:{x} y:{y}</p>
         </div>
     )
 }
