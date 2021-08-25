b31ef308b284abb994d3df244de095d19be8c908
issyrivas17 <ismeniarivas17@gmail.com>
2021-08-24 21:46:21 -0400

    Usefetch mas useCounter

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -1,15 +1,48 @@
 import React from 'react'
 import '../02-useEffect/effects.css'
+import { useCounter } from '../hooks/useCounter'
 import { useFetch } from '../hooks/useFetch'
 export const MultipleCustomHooks = () => {
 
-const state= useFetch(`https://www.breakingbadapi.com/api/quotes/1`)
-console.log(state)
+const {counter,increment}=useCounter(1)
+
+
+const{loanding,data}= useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`)
+
+
+const {author,quote} = !! data && data[0];  
+
+
 
 
     return (
         <div>
-            <h1>Custom Hooks!!!!</h1> 
+            <h1>BreakingBad Quotes</h1> 
+           <hr/> 
+{
+    loanding 
+    ? 
+    (
+          <div className='alert alert-info text-center'> 
+          Loanding...
+        </div> 
+    ) : 
+    (
+        <blockquote className = 'blockquote text-right'>
+        <p className ='mb-0'>{quote} </p>  
+        <footer className='blockquote-footer'> {author}.</footer>
+        </blockquote> 
+
+    )
+} 
+
+<button 
+className ='btn btn-primary'
+onClick = {increment}
+>
+    Siguiente frase. 
+</button>
+
         </div>
     )
 }
--- src/components/hooks/useFetch.js
+++ src/components/hooks/useFetch.js
@@ -6,11 +6,12 @@ export const useFetch = (url) => {
     const [state, setState] = useState({data: null,loanding:true,error: null})
 
   useEffect(() => {
+    setState({data: null, loanding: true , error:null})
 fetch (url)
 .then (resp => resp.json()) 
 .then(data => {
 
-    setState ({
+    setState ({ 
         loanding: false,
         error:null,
         data
