916b40a5fca910b607dd723303b26db7a6b1ef3a
issyrivas17 <ismeniarivas17@gmail.com>
2021-08-25 21:48:18 -0400

    useRef

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; 
-import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+import { FocusScreen } from './components/04-useRef/FocusScreen';
+
+/* import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks'; */
 /* import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook'; */
 /* import { SimpleForm } from './components/02-useEffect/SimpleForm'; */
 /* import {HookApp} from './HookApp' */ 
@@ -12,7 +14,7 @@ import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHook
 
 ReactDOM.render(
  
-    <MultipleCustomHooks/>, 
+    <FocusScreen/>, 
   document.getElementById('root')
 );
 
