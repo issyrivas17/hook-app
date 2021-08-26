9ebb7913a0fb3da36abd785565372c529e231a1f
issyrivas17 <ismeniarivas17@gmail.com>
2021-08-25 22:20:18 -0400

    Se debe arreglar un problema que tiene el useEffect con una coma

--- src/components/hooks/useFetch.js
+++ src/components/hooks/useFetch.js
@@ -1,9 +1,19 @@
-import {useState,useEffect} from 'react'
+import {useState,useEffect,useRef} from 'react'
 
 export const useFetch = (url) => {
-    
+
+    const isMounted = useRef(true); 
 
     const [state, setState] = useState({data: null,loanding:true,error: null})
+
+    useEffect =( () => {
+      return () => {
+        isMounted.current = false; 
+      }
+
+    },[])
+
+
 
   useEffect(() => {
     setState({data: null, loanding: true , error:null})
@@ -11,11 +21,20 @@ fetch (url)
 .then (resp => resp.json()) 
 .then(data => {
 
-    setState ({ 
-        loanding: false,
-        error:null,
-        data
-    })
+setTimeout(() => { 
+   if(isMounted.curr){
+     setState ({ 
+         loanding: false,
+         error:null,
+         data
+     })
+
+   }
+
+}, 4000);
+
+
+
 })
   },[url]) 
 
