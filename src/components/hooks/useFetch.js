a71b0e2b6d8a1ed23c37818b675153c9989fac75
issyrivas17 <ismeniarivas17@gmail.com>
2021-08-29 15:22:52 -0400

    Era un error de tipeo

--- src/components/hooks/useFetch.js
+++ src/components/hooks/useFetch.js
@@ -1,42 +1,32 @@
-import {useState,useEffect,useRef} from 'react'
+ 
+ import {useState,useEffect,useRef} from 'react'
 
-export const useFetch = (url) => {
+ export const useFetch = (url) => {
+  
+  const isMounted = useRef(true); 
+const [state, setState] = useState({data: null,loanding:true,error: null})
 
-    const isMounted = useRef(true); 
+useEffect( () => {
+  return () => {
+    isMounted.current = false; 
+  }
+}, [])
 
-    const [state, setState] = useState({data: null,loanding:true,error: null})
+useEffect(() => {
 
-    useEffect =( () => {
-      return () => {
-        isMounted.current = false; 
-      }
-
-    },[])
-
-
-
-  useEffect(() => {
-    setState({data: null, loanding: true , error:null})
-fetch (url)
-.then (resp => resp.json()) 
-.then(data => {
-
-setTimeout(() => { 
-   if(isMounted.curr){
-     setState ({ 
-         loanding: false,
-         error:null,
-         data
-     })
-
-   }
-
-}, 4000);
-
-
-
-})
-  },[url]) 
-
-  return state; 
-}
+setState({data: null, loanding: true , error:null}); 
+  
+fetch(url)
+  .then (resp => resp.json()) 
+  .then(data => {
+    if(isMounted.current) {
+      setState ({ 
+          loanding: false,
+          error:null,
+          data
+         });
+     } 
+},)  
+},[url])
+return state;
+ }
