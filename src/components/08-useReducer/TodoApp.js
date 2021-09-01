f77cb57cc30c1041cb9f3c6582439ef0d723b88d
issyrivas17 <ismeniarivas17@gmail.com>
2021-08-31 22:37:36 -0400

    Maquetacion de todoApp

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -12,18 +12,55 @@ export const TodoApp = () => {
 
 const [todos] = useReducer(todoReducer, initialState) 
 
-console.log(todos)
+console.log(todos); 
 
     return (
-        <>
-            <h1>TodoApp</h1> 
-            <hr/> 
+                <>
+                    <h1>TodoApp ({todos.length})</h1> 
+                    <hr/> 
 
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li> 
-                <li>Nuevo!!</li>
-            </ul>
-        </>
+                    <div className ='row'> 
+                    <div className = 'col-7'> 
+                    <ul className ='list-group list-group-flush' >
+                    {
+                        todos.map ( (todo,i) => (
+                            <li 
+                            key= {todo.id}
+                            className= 'list-group-item'
+                            > 
+                                <p className ='text-center'>{i+1}.{todo.desc} </p>
+                                
+                                <button
+                                className = 'btn btn-danger'
+                                >Borrar</button>
+                                
+                                </li>
+                        ))
+                    }
+                    </ul>
+                    </div>
+
+                <div className = 'col-5'>
+                <h4> Agregar TODO</h4> 
+                <hr/> 
+                <form>
+                    <input
+                    className = 'form-control'
+                    type= 'text'
+                    name= 'description' 
+                    placeholder = 'Aprender...'
+                    autoComplete ='off'
+                    /> 
+                    <button 
+                    className= 'btn btn-outline-primary mt-1 block-buttons'> 
+                    Agregar
+                    </button>
+                </form>
+
+
+                </div>
+                
+                    </div>
+                </>
     )
 }
