56e5d5a30d8b3d4bf586fb9e0f3290ab322ec245
issyrivas17 <ismeniarivas17@gmail.com>
2021-09-05 16:56:01 -0300

    Optimizando el codigo parte 1

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -2,6 +2,7 @@ import React, { useEffect, useReducer } from 'react'
 import'./style.css'
 import { todoReducer } from './todoReducer'; 
 import {useForm} from '../hooks/useForm' 
+import { Todolist } from './Todolist'; 
 
 export const TodoApp = () => {
 
@@ -69,29 +70,12 @@ reset ();
                     <div className ='row'> 
                     <div className = 'col-7'> 
                     {/*TodoList,Todos,handleDelete,handToggle*/}
-                    <ul className ='list-group list-group-flush' >
-                    {
-                        todos.map ( (todo,i) => ( 
-                            //TodoListItem, todo, index, handleDelete, handleToggle
-
-                            <li 
-                            key= {todo.id}
-                            className= 'list-group-item'
-                            > 
-                                <p 
-                                className= {`${todo.done && 'complete'}` }
-                                onClick={ ()=>handleToggle(todo.id)}
-                                >{i+1}.{todo.desc} </p>
-                                
-                                <button
-                                className = 'btn btn-danger'
-                                onClick= {()=> handleDelete(todo.id)}
-                                >Borrar</button>
-                                
-                                </li>
-                        ))
-                    }
-                    </ul>
+                   <Todolist
+                   todos= {todos} 
+                   handleDelete= {handleDelete} 
+                   handleToggle= {handleToggle}
+                   
+                   /> 
                     </div>
 
                 <div className = 'col-5'>
