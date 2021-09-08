f1057de8a0ab984b37a1a97520d048e3acab68dc
issyrivas17 <ismeniarivas17@gmail.com>
2021-09-07 21:41:03 -0300

    Preparacion para aplicacion con rutas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
  import React from 'react';
 import ReactDOM from 'react-dom'; 
-import { TodoApp } from './components/08-useReducer/TodoApp';
+import { MainApp } from './components/09-useContex/MainApp';
+/* import { TodoApp } from './components/08-useReducer/TodoApp'; */
 /* import { CallbackHook } from './components/06-memos/CallbackHook'; */
 /* import { Padre } from './components/07-tarea-memo/Padre'; */
 /* import { MemoHook } from './components/06-memos/MemoHook'; */
@@ -21,7 +22,7 @@ import { TodoApp } from './components/08-useReducer/TodoApp';
 
  ReactDOM.render(
  
-    <TodoApp/>, 
+    <MainApp/>, 
   document.getElementById('root')
 );
 
