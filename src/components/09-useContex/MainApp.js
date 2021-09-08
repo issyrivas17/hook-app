00940051e0a405c7a63f8cb5b65aeefc05e49d8c
issyrivas17 <ismeniarivas17@gmail.com>
2021-09-07 22:04:24 -0300

    Configuracion de router en react

--- src/components/09-useContex/MainApp.js
+++ src/components/09-useContex/MainApp.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { AppRouter } from './AppRouter'
 
 export const MainApp = () => {
     return (
-        <>
-           <h1>MainApp</h1>  
-          <hr/>  
-        </>
+          <AppRouter/> 
     )
+
+
+
+
+
 }
