cd4e91a782d99e5bd469427df01df9964686cd90
issyrivas17 <ismeniarivas17@gmail.com>
2021-09-07 22:16:00 -0300

    Empezando link y NavLink, faltan acomodar detalles

--- src/components/09-useContex/AppRouter.js
+++ src/components/09-useContex/AppRouter.js
@@ -7,19 +7,22 @@ import {
 import { AboutScreen } from './AboutScreen';
 import { HomeScreen } from './HomeScreen';
 import { LoginScreen } from './LoginScreen';
+import { NavBar } from './NavBar';
 
 export const AppRouter = () => {
     return (
     <Router>
         <div>
-            <switch> 
+
+            
+            <Switch> 
+
             <Route exact path='/' component = {HomeScreen} />
             <Route exact path='/about' component = {AboutScreen} />
             <Route exact path='/login' component = {LoginScreen} />
-         
-
-
-            </switch>
+        
+            </Switch>
+            <NavBar/>  
         </div>
     </Router>
     )
---
+++ src/components/09-useContex/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                    <li>
+                        <a href='./'>Home</a>
+                    </li>  
+                
+                    <li>
+                    <a href='./about'>About</a>
+                        </li>  
+
+                    <li>
+                    <a href='./login'>Login</a>
+                        </li>
+            </ul>
+        </nav>
+    )
+}
